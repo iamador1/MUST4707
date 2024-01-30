202f8523727b60edb4132309124e33b0bce797c3
iamador1 <156848531+iamador1@users.noreply.github.com>
2024-01-30 14:14:58 -0500

    Update script.js

--- In-Class.Code/Week.2/01.reviewObjects/script.js
+++ In-Class.Code/Week.2/01.reviewObjects/script.js
@@ -1,1 +1,13 @@
 //Let's make some objects again!
+const apple = {
+    color: "red",
+    flavorRating: 10,
+    variety: "Honeycrisp",
+    stolen: true,
+    howTasty: function (text) {
+        console.log(
+            `This apple is a ${this.flavorRating} out of 10 in terms of tastiness`
+        );
+    },
+
+}
